b174464f06e67d3ef23861072ec2ebea5665199f
Ryan Gibson <ryan.l.gibson@protonmail.com>
2025-06-17 22:59:56 -0400

    Created function to call unsplash api to fetch random images and set as quote card background.

--- public/js/index.js
+++ public/js/index.js
@@ -1,40 +1,18 @@
-"use strict"
+"use strict";
 
-const elements = {
-    quote: document.getElementById("quote"),
-    author: document.getElementById("author")
+async function getRandomImage() {
+  const client_id = "ACCESS_TOKEN";
+  const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
+  try {
+    const response = await fetch(endpoint);
+    const returnedData = await response.json();
+    const receivedPhotoURL = returnedData.urls.regular;
+
+    const imgDiv = document.querySelector(".background-img");
+    imgDiv.style.backgroundImage = `url("${receivedPhotoURL}")`;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-const quotes = [
-    {
-        quote: "All hands! Abandon ship!",
-        author: "Captain Picard",
-    },
-
-    {
-        quote: "Doh!",
-        author: "Homer Simpson",
-    },
-
-    {
-        quote: "The Internet is the first thing that humanity has built that humanity doesn't understand, the largest experiment in anarchy that we have ever had.",
-        author: "Eric Schmidt",
-    }
-];
-
-
-function loopThroughQuotes() {
-    let quoteIndex = 0;
-    setInterval(() => {
-        if (quoteIndex < quotes.length) {
-            elements.quote.textContent = quotes[quoteIndex].quote;
-            elements.author.textContent = quotes[quoteIndex].author;
-            quoteIndex++;
-        } else {
-            quoteIndex = 0;
-        }
-    }, 3000);
-}
-
-
-setTimeout(loopThroughQuotes, 3000);
+getRandomImage();
