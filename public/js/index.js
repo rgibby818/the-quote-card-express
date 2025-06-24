fccd002f69c8031799435870c1003f12b102b8bd
Ryan Gibson <ryan.l.gibson@protonmail.com>
2025-06-23 23:06:39 -0400

    added api call for getting random images for quote card

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,14 @@
 "use strict";
 
 async function getRandomImage() {
-  const client_id = "ACCESS_TOKEN";
-  const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
+  const endpoint = `http://localhost:8080/api/v1/getRandomImage`;
   try {
     const response = await fetch(endpoint);
+    if(!response.ok) {
+        throw new Error(`${endpoint} failed to fetch`);
+    }
     const returnedData = await response.json();
-    const receivedPhotoURL = returnedData.urls.regular;
+    const receivedPhotoURL = returnedData.data;
 
     const imgDiv = document.querySelector(".background-img");
     imgDiv.style.backgroundImage = `url("${receivedPhotoURL}")`;
--- server.js
+++ server.js
@@ -4,6 +4,20 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
+require("dotenv").config();
+const cors = require("cors");
+
+const corsOptions = {
+  origin: `http://localhost:${port}`
+};
+app.use(cors());
+app.use("/api/v1/getRandomImage", async (request, response) => {
+  response.status(200).json({
+    status: 200,
+    data: await getRandomImage(),
+  });
+});
+
 app.use(express.static("./public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -11,3 +25,18 @@ app.use(express.urlencoded({ extended: false }));
 app.listen(port, () => {
   console.log(`Server is running http://localhost:${port}`);
 });
+
+
+
+async function getRandomImage() {
+    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${process.env.CLIENT_ID}`;
+    try {
+        const response = await fetch(endpoint);
+        const returnedData = await response.json();
+        const receivedPhotoUrl = returnedData.urls.regular;
+
+        return receivedPhotoUrl;
+    } catch (error) {
+        console.error(error);
+    }
+  }
